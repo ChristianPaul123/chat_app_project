ce87ab6996efbb8df62c088f66d9d929a3995537
ChristianPaul123 <christianpaulespares1@gmail.com>
2023-05-19 08:25:47 +0800

    version final

--- public/code.js
+++ public/code.js
@@ -53,7 +53,7 @@
 
   app.querySelector(".chat-screen #exit-chat").addEventListener("click",function(){ 
     socket.emit("exituser", uname);
-    window.location.href = window.location.href;
+   window.location.href = window.location.origin;
   });
 
   window.addEventListener('onoffline', function() {
